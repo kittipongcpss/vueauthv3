5062516f5db37fd94e6be70dc924cd78bdbea6c7
kittipongcpss <129354238+kittipongcpss@users.noreply.github.com>
2024-07-22 20:14:39 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDgfEZ-EwkaDuURelY7K32o1d2LIhbvBs8",
-  authDomain: "iotcom-dbc7b.firebaseapp.com",
-  databaseURL: "https://iotcom-dbc7b-default-rtdb.firebaseio.com",
-  projectId: "iotcom-dbc7b",
-  storageBucket: "iotcom-dbc7b.appspot.com",
-  messagingSenderId: "738935722385",
-  appId: "1:738935722385:web:1d1cccfe557cfbc5fd8a30",
-  measurementId: "G-1QS632QHN8",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
   };
 
 
